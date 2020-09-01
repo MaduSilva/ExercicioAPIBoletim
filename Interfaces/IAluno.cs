3682f2284896e8bad960766e96f45da0040eb65b
FIC <FIC@info.local>
2020-09-01 19:26:01 -0300

    Exercicio Finalizado

---
+++ Interfaces/IAluno.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using APIBoletim.Domains;
+
+namespace APIBoletim.Interfaces
+{
+    interface IAluno
+    {
+        Aluno Cadastrar(Aluno a);
+        List<Aluno> LerTodos();
+        Aluno BuscarId(int id);
+        Aluno Alterar(Aluno a);
+        Aluno Excluir(Aluno a);
+   
+    }
+}
